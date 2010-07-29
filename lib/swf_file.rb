0194ebd80d604cbd1c2d777b0916118688c57651
Estanislau Trepat <estanis@Apollo.(none)>
2010-07-29 17:33:07 +0200

    added remote file testing by checking response

--- lib/swf_file.rb
+++ lib/swf_file.rb
@@ -1,5 +1,6 @@
 $:.unshift File.join(File.dirname(__FILE__), 'swf_file') unless $:.include? File.join(File.dirname(__FILE__), 'swf_file')
 
+require 'net/http'
 require 'uri'
 require 'open-uri'
 require 'zlib'
@@ -13,28 +14,42 @@ require 'swf_header'
 class SwfFile
   # Class methods
   def self.header(swf_path)
-    # TODO: improve URI/URL validation by actually checking if the resource exists (request responds)
-    raise RuntimeError, "SWF file not found.", caller if !File.exists?(swf_path) && URI.extract(swf_path).empty?
-    
+    raise RuntimeError, "SWF file not found.", caller if !self.swf_exists?(swf_path)
+
     header = SwfHeader.new(swf_path)
     yield(header) if block_given?
     header
   end
-  
+
+  def self.swf_exists?(swf_path)
+    return true if File.exists?(swf_path)
+
+    begin
+      case Net::HTTP.get_response(URI.parse(swf_path))
+        when Net::HTTPSuccess
+          true
+        else
+          false
+      end
+    rescue Exception => e
+      false
+    end
+  end
+
   # Instance methods
   def initialize(swf_path)
-    # TODO: improve URI/URL validation by actually checking if the resource exists (request responds)
-    raise RuntimeError, "SWF file not found.", caller if !File.exists?(swf_path) && URI.extract(swf_path).empty?
-    
+    raise RuntimeError, "SWF file not found.", caller if !SwfFile.swf_exists?(swf_path)
+
     @header = SwfHeader.new(swf_path)
   end
-  
+
   def header
     yield(@header) if block_given?
     @header
   end
-  
+
   def compressed?
     @header.compressed?
   end
 end
+
--- test/unit/swf_file_test.rb
+++ test/unit/swf_file_test.rb
@@ -9,11 +9,15 @@ class SwfFileTest < Test::Unit::TestCase
       should "not be initializable without an SWF file path" do
         assert_raise(ArgumentError) { SwfFile.new }
       end
-    
+
       should "raise an error if an invalid path is provided" do
         assert_raise(RuntimeError) { SwfFile.new "/path_to_imaginary_file" }
       end
-      
+
+      should "raise an error if an invalid uri is provided" do
+        assert_raise(RuntimeError) { SwfFile.new "http://www.blah.com/dummy_swf" }
+      end
+
       should "return a header with the regular properties" do
         swf = SwfFile.new fixture_path('clicktag.swf')
         assert_equal SwfHeader, swf.header.class
@@ -21,87 +25,88 @@ class SwfFileTest < Test::Unit::TestCase
         assert_equal swf.compressed?, swf.header.compressed?
       end
     end # instance methods
-    
-    context "class method header" do      
+
+    context "class method header (local)" do
       should "raise an error when not provided with an SWF" do
         assert_raise(RuntimeError) { SwfFile.header fixture_path('smallgoat.jpg') }
       end
-      
+
       setup do
         @header = SwfFile.header fixture_path('clicktag.swf')
       end
-      
+
       should "return an instance of SwfHeader" do
         assert_equal SwfHeader, @header.class
       end
-      
+
       should "indicate the file's size" do
         assert_equal 4156, @header.size
       end
-      
+
       should "state if the swf was originally compressed" do
         assert @header.compressed?
         assert !SwfFile.header(fixture_path('clicktag-decompressed.swf')).compressed?
       end
-      
+
       should "not make available private compression module methods" do
         %w{ :buffer_compressed? :strip_buffer_header :decompress_buffer! }.each do |method|
           assert !@header.respond_to?(method)
         end
       end
-      
+
       should "state if its buffer is currently compressed" do
         assert @header.compressed?
         assert !@header.compressed?(false)
       end
-      
+
       should "have the version" do
         assert_equal 8, @header.version
       end
-      
+
       should "have the bit count" do
         assert_equal 15, @header.bit_count
       end
-      
+
       should "have the xmax and ymax values" do
         assert_equal 9360, @header.xmax
         assert_equal 1200, @header.ymax
       end
-      
+
       should "have the width and height values" do
         assert_equal 468, @header.width
         assert_equal 60, @header.height
       end
-      
+
       should "have the frame rate and frame count" do
         assert_equal 24, @header.frame_rate
         assert_equal 1, @header.frame_count
       end
-      
+
       should "include the duration of the swf file" do
         assert_equal 42, @header.duration
       end
-      
+
       should "contain the avm version" do
         assert_equal 'AVM1', @header.avm_version
       end
-      
+
       should "include the swf signature" do
         assert_equal 'CWS', @header.signature
       end
-      
+
       should "output basing information when to_s is invoked" do
         assert @header.to_s =~ /.+clicktag.swf\s\d+x\d+\s\d\dfps/
       end
-      
+
       should "be have a hash converter" do
         assert_equal Hash, @header.to_hash.class
       end
-      
+
       should "have the same output for the instance methods inspect and to_hash" do
         assert_equal @header.to_hash, @header.inspect
       end
     end # SWF operations context
-    
+
   end # SwfFile context
 end # SwfFileTest
+
